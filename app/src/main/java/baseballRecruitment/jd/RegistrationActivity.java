6ef9a5fee159c4e8575cb2585e91767ae85e7f38
Charles Cagle <cjcaglejr@gatech.edu>
2017-11-27 14:34:04 +0000

    constraint login and register

--- app/src/main/java/baseballRecruitment/jd/RegistrationActivity.java
+++ app/src/main/java/baseballRecruitment/jd/RegistrationActivity.java
@@ -2,6 +2,7 @@ package baseballRecruitment.jd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void register(View view) {
+        finish(); // TODO
     }
 }
