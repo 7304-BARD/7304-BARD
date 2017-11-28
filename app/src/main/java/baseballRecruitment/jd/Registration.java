8073469d9c31a8d6704d8fcff9d56ac677247562
Sergey Nall <snall8@gatech.edu>
2017-11-28 11:32:54 -0500

    Registration
    You can register a user now.

--- app/src/main/java/baseballRecruitment/jd/Registration.java
+++ app/src/main/java/baseballRecruitment/jd/Registration.java
@@ -3,6 +3,9 @@ package baseballRecruitment.jd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.arch.persistence.room.Room;
+import android.widget.TextView;
+import baseballRecruitment.jd.DataLayer.Account.*;
 
 public class Registration extends AppCompatActivity {
 
@@ -13,6 +16,23 @@ public class Registration extends AppCompatActivity {
     }
 
     public void register(View view) {
+        // TODO Encapsulate later
+        final String email = ((TextView) findViewById(R.id.email)).getText().toString();
+        final String username = ((TextView) findViewById(R.id.email)).getText().toString();
+        final String password = ((TextView) findViewById(R.id.password)).getText().toString();
+        final String password_confirm = ((TextView) findViewById(R.id.confirm_password)).getText().toString();
+        final String organization = ((TextView) findViewById(R.id.org_response)).getText().toString();
+
+        if (!password.equals(password_confirm))
+            ; // TODO: admonish user
+
+        Account acc = new Account(email, username, password, organization);
+        AccountsDatabase db =
+                Room.databaseBuilder(getApplicationContext(), AccountsDatabase.class, "accounts")
+                        .allowMainThreadQueries().build();
+        db.userDao().insertAccounts(acc);
+
+        // TODO: congratulate user
         finish(); // TODO
     }
 }
