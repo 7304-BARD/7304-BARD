9d8ef465bfd9987cb3321524b7548780474ffa48
Charles Cagle <cjcaglejr@gatech.edu>
2017-12-03 19:42:36 +0000

    tweak calendar interface

--- app/src/main/java/baseballRecruitment/jd/MyCalendar.java
+++ app/src/main/java/baseballRecruitment/jd/MyCalendar.java
@@ -1,7 +1,9 @@
 package baseballRecruitment.jd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyCalendar extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MyCalendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_calendar);
+    }
+
+    void new_meeting(View view) {
+        startActivityForResult(new Intent(this, NewMeeting.class), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int request, int result, Intent data) {
     }
 }
--- app/src/main/java/baseballRecruitment/jd/NewMeeting.java
+++ app/src/main/java/baseballRecruitment/jd/NewMeeting.java
@@ -1,13 +1,107 @@
 package baseballRecruitment.jd;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class NewMeeting extends AppCompatActivity {
+
+    Calendar date;
+    Calendar start;
+    Calendar end;
+    String details = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_meeting);
+        date = Calendar.getInstance();
+        start = Calendar.getInstance();
+        end = Calendar.getInstance();
+        ((EditText) findViewById(R.id.meeting_info)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                details = editable.toString();
+            }
+        });
+    }
+
+    void submit(View view) {
+        Intent result = new Intent();
+        result.putExtra("date", date);
+        result.putExtra("start", start);
+        result.putExtra("end", end);
+        result.putExtra("details", details);
+        setResult(Activity.RESULT_OK, result);
+        finish();
+    }
+
+    public void setDate(final View view) {
+        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                date.set(Calendar.YEAR, year);
+                date.set(Calendar.MONTH, month);
+                date.set(Calendar.DAY_OF_MONTH, day);
+                displayDate((EditText) view, date);
+            }
+        }, date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    public void setTime(View v, Calendar cal) {
+        new TimePickerDialog(this, new OTSL((EditText) v, cal), 0, 0, false).show();
+    }
+
+    public void setStartTime(View v) {
+        setTime(v, start);
+    }
+
+    public void setEndTime(View v) {
+        setTime(v, end);
+    }
+
+    void displayTime(EditText et, Calendar cal) {
+        et.setText(DateFormat.getTimeFormat(this).format(cal.getTime()));
+    }
+
+    void displayDate(EditText et, Calendar cal) {
+        et.setText(DateFormat.getDateFormat(this).format(cal.getTime()));
+    }
+
+    private class OTSL implements TimePickerDialog.OnTimeSetListener {
+        EditText et;
+        Calendar cal;
+
+        OTSL(EditText et, Calendar cal) {
+            this.et = et;
+            this.cal = cal;
+        }
+
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+            cal.set(Calendar.HOUR, hour);
+            cal.set(Calendar.HOUR, minute);
+            displayTime(et, cal);
+        }
     }
 }
