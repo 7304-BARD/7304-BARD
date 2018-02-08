8fdd374b08ebc4f29e4ac328270e212f305806bc
Sergey Nall <snall8@gatech.edu>
2018-02-08 14:54:17 -0500

    IOS Top 50 HS
    Allows user to choose which year to get the top 50 high school players
    from.

---
+++ IOS/Baseball Recruitment/Baseball Recruitment/Top50HighSchool.swift
@@ -0,0 +1,143 @@
+//
+//  Top50HighSchool.swift
+//  Baseball Recruitment
+//
+//  Created by Sergey Scott Nall  on 2/7/18.
+//  Copyright © 2018 Team Bard. All rights reserved.
+//
+
+import UIKit
+import Kanna
+import Alamofire
+
+class Top50HighSchool
+{
+    struct Player
+    {
+        // These are the values that are scraped from the top 50 screen.
+        var name : String
+        var position : String
+        var href: String
+        var commitment : String?
+    }
+    
+    //MARK: Get Valid 'Class of' year labels.
+    func requestValidYears(completion : @escaping ([String]?) -> ()) {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy"
+        let year = dateFormatter.string(from: date)
+        
+        let mainUrl = "https://www.perfectgame.org/"
+        let subUrl = "Rankings/Players/NationalRankings.aspx?gyear=\(year)"
+        let url = mainUrl + subUrl
+        
+        Alamofire.request(url).responseString{ response in
+            guard response.result.isSuccess else {
+                print("Alamofire.request failed.\n\(String(describing: response.error))")
+                return
+            }
+            
+            if let html = response.result.value {
+                completion(self.scrapeValidYears(html: html))
+            }
+        }
+    }
+    
+    func scrapeValidYears(html: String) -> [String] {
+        var validYears = [String]()
+        
+        guard let doc = Kanna.HTML(html: html, encoding: String.Encoding.utf8)
+            else {
+                print("Kana.HTML failed.")
+                return validYears
+        }
+        
+        let selector = "#ctl00_ContentPlaceHolder1_RadComboBox1_DropDown"
+        let nodes = doc.css(selector)
+        
+        if nodes.count < 1 {
+            return validYears
+        }
+        
+        let node = nodes[0]
+        let liNodes = node.css("li")
+        
+        
+        for liNode in liNodes {
+            validYears.append(liNode.text ?? "")
+            //print(liNode.text ?? "")
+        }
+        
+        return validYears
+    }
+    
+    //MARK: Get top 50 students for a particular year
+    func requestTopHighSchool(year: Int = 2020, completion : @escaping (Int, [Player]) -> ()) {
+        let mainUrl = "https://www.perfectgame.org/"
+        let subUrl = "Rankings/Players/NationalRankings.aspx?gyear=\(year)"
+        let url = mainUrl + subUrl
+        Alamofire.request(url).responseString{ response in
+            guard response.result.isSuccess else {
+                print("Alamofire.request failed.")
+                return
+            }
+            
+            if let html = response.result.value {
+                let players = self.scrapeTopHighSchool(year: year, html: html)
+                completion(year, players)
+            }
+        }
+    }
+    
+    func scrapeTopHighSchool(year: Int, html: String) -> [Player] {
+        var players = [Player]()
+        
+        guard let doc = Kanna.HTML(html: html, encoding: String.Encoding.utf8)
+            else {
+                print("Kana.HTML failed.")
+                return players
+        }
+        
+        let tableSelector = "table[id='ContentPlaceHolder1_gvPlayers']"
+        let tables = doc.css(tableSelector)
+        
+        guard tables.count > 0 else {
+            return players
+        }
+        
+        let table = tables[0]
+        let rows = table.css("tr")
+        let nRows = rows.count
+        
+        guard nRows > 0 else {
+            return players
+        }
+        
+        for i in 1...nRows {
+            if i % 2 == 0 {
+                continue
+            }
+            
+            let row = rows[i]
+            
+            let aNode1 = row.at_css("a[href*='Players/Playerprofile.aspx?ID=']")
+            let href = aNode1?["href"] ?? ""
+            let playerName = aNode1?.text ?? ""
+            
+            let tdNode = row.at_css("td:nth-child(3)")
+            let position = tdNode?.text ?? ""
+            
+            let aNode2 = row.at_css("a[id^='ContentPlaceHolder1_gvPlayers_hl4yr_']")
+            let commitment = aNode2?.text
+            
+            players.append(Player(name: playerName, position: position,
+                                  href: href, commitment: commitment))
+            //print("\(playerName),\(position),\(commitment),\(href)")
+        }
+        
+        return players
+    }
+
+    
+}
--- IOS/Baseball Recruitment/Baseball Recruitment/Top50ViewController.swift
+++ IOS/Baseball Recruitment/Baseball Recruitment/Top50ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class Top50ViewController: UIViewController {
+class Top50ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     //MARK: Properties
-    @IBOutlet weak var yearPicker: UIPickerView!
+    var selectedYear = "Select a class year"
+    
+    @IBOutlet weak var yearPicker: UITableView!
     
     
     //MARK: override functions
@@ -25,15 +27,58 @@ class Top50ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //MARK: TableView functions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Top50ClassOf"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = selectedYear
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    
+    // MARK: Navigation
 
     /*
-    // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    //MARK: Actions
+    @IBAction func unwindToTop50(sender: UIStoryboardSegue) {
+        guard let sourceViewController = sender.source as?
+            ClassOfTableViewController else {
+            return
+        }
+        
+        guard let index = sourceViewController.tableView.indexPathForSelectedRow
+            else {
+            return
+        }
+        
+        selectedYear = sourceViewController.classOfYears[index.row]
+        let index2 = yearPicker.indexPathsForSelectedRows!
+        yearPicker.reloadRows(at: index2, with: .automatic)
+    }
+    
+
 
 }
